68a303bdc9bfd4005a5ef07d635d134eadd8941b
Sylvere Armange <sylvere.armange@gmail.com>
2019-04-09 11:28:59 +0200

    palettes

---
+++ Assets/Scripts/EnemyA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyA : Character2D
+{
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
